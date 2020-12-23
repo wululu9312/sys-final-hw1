437a83aec969ec2139b6b141326f3ad306d5828a
root <iva10312@gmail.com>
2020-12-23 21:27:21 +0800

    test01

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
+import time
+import redis
 from flask import Flask
-from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host="redis")
+cache = redis.Redis(host='redis', port=6379)
 
-@app.route("/")
-def hello():
-    visits = redis.incr('counter')
-    html ="<h3>Hello World!</h3>" \
-          "<b>Visits:</b> {visits}"\
-          "<br/>"
-    return html.format(visits=visits)
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc: 
+            if retries == 0:
+                raise exc
+            retries -= 1 
+            time.sleep(0.5)
+
+@app.route('/')
+def get_index():
+    count = get_hit_count()
+    return 'Yo! 你是第 {} 次瀏覽\n'.format(count)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=80)
-
+    app.run(host="0.0.0.0", debug=True)
